cf4b2a365719a6470bf1e197932f444e52f108f9
Difenter <afonking.denis13@gmail.com>
2023-06-13 16:26:09 +0300

    refactor

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,192 +1,113 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import Backendless from "backendless";
 
 const Home = () => {
 
-    Backendless.serverURL = "https://api.backendless.com";
-    Backendless.initApp('88F13D1C-FC0C-2D37-FF91-A541959F4400', '4D32BF48-EE07-4934-B468-177BA8E02A23');
+  Backendless.serverURL = "https://api.backendless.com";
+  Backendless.initApp('88F13D1C-FC0C-2D37-FF91-A541959F4400', '4D32BF48-EE07-4934-B468-177BA8E02A23');
 
-    const [currentUser, setCurrentUser] = useState<any>()
+  const [currentUser, setCurrentUser] = useState<any>()
 
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-    const [name, setName] = useState("")
-    const [age, setAge] = useState("")
-    const [country, setCountry] = useState("")
-    const [gender, setGender] = useState("")
-    const reg = () => {
-        const user = new Backendless.User()
-        user.email = email
-        user.password = password
-        // @ts-ignore
-        user.name = name
-        // @ts-ignore
-        user.age = age
-        // @ts-ignore
-        user.country = country
-        // @ts-ignore
-        user.gender = gender
-        Backendless.UserService.register(user).then((response) => {
-            Backendless.Files.upload("reg.txt", `users/${name}`)
-            Backendless.Files.upload("reg.txt", `users/${name}/sharedWithMe`)
-        })
-    }
+  const [image, setImage] = useState<any>("")
 
-    const [emailLog, setEmailLog] = useState("")
-    const [passwordLog, setPasswordLog] = useState("")
+  const handleFile = () => {
+    Backendless.Files.upload(image, `users/${currentUser.name}`).then((response) => {
+      console.log(response)
+    })
+  }
 
-    const log = () => {
-        Backendless.UserService.login(emailLog, passwordLog, true).then((response: any) => {
-            console.log(response)
-        }).catch((e) => {
-            alert(e)
-        })
-    }
+  const [nameFolder, setNameFolder] = useState("")
 
-    // const log = () => {
-    //     let online = 0
-    //     Backendless.Data.of("Statistic").findById({objectId: "EBC44CCC-B3FF-4F67-881B-D2F6CF913089"})
-    //         .then((response) => {
-    //             // @ts-ignore
-    //             console.log(response.online)
-    //             // @ts-ignore
-    //             online = response.online
-    //             const r = Backendless.UserService.login(emailLog, passwordLog, true).then((response2) => {
-    //                 // @ts-ignore
-    //                 console.log(online)
-    //                 // @ts-ignore
-    //                 console.log(response.online)
-    //                 Backendless.Data.of("Statistic").save({
-    //                     objectId: "EBC44CCC-B3FF-4F67-881B-D2F6CF913089",
-    //                     // @ts-ignore
-    //                     online: online + 1
-    //                 })
-    //             })
-    //         })
-    // }
+  const createFolder = () => {
+    Backendless.Files.upload("reg.txt", `users/${currentUser.name}/${nameFolder}`).then((response) => {
 
+    }).catch((e) => {
+      alert(e)
+    })
+  }
 
+  const [nameFolderDelete, setNameFolderDelete] = useState("")
 
+  const deleteFolder = () => {
+    Backendless.Files.remove(`users/${currentUser.name}/${nameFolderDelete}`)
+  }
 
-    const help = () => {
-        Backendless.UserService.restorePassword(email).then((response) => {
-            console.log(response)
-        })
-    }
+  const [list, setList] = useState<any>()
 
-    const [image, setImage] = useState<any>("")
+  const fetchList = () => {
+    Backendless.Files.listing(`users/${currentUser.name}/sharedWithMe`).then((response) => {
+      setList(response)
+    })
+  }
+  console.log('list', list)
 
-    console.log(name)
+  useEffect(() => {
+    Backendless.UserService.isValidLogin().then(response => {
+      console.log('isValidLogin', response)
+    })
+  }, [])
 
-    const handleFile = () => {
-        Backendless.Files.upload(image, `users/${currentUser.name}`).then((response) => {
-            console.log(response)
-        })
-    }
+  useEffect(() => {
+    Backendless.UserService.getCurrentUser().then((response: any) => {
+      console.log('getCurrentUser', response)
+      setCurrentUser(response)
+    })
+  }, [])
 
-    const [nameFolder, setNameFolder] = useState("")
-
-    const createFolder = () => {
-        Backendless.Files.upload("reg.txt", `users/${currentUser.name}/${nameFolder}`).then((response) => {
-
-        }).catch((e) => {
-            alert(e)
-        })
-    }
-
-    const [nameFolderDelete, setNameFolderDelete] = useState("")
-
-    const deleteFolder = () => {
-        Backendless.Files.remove(`users/${currentUser.name}/${nameFolderDelete}`)
-    }
-
-    const [list, setList] = useState<any>()
-
-    const fetchList = () => {
-        Backendless.Files.listing(`users/${currentUser.name}/sharedWithMe`).then((response) => {
-            setList(response)
-        })
-    }
-    console.log(list)
-
-    useEffect(() => {
-        Backendless.UserService.isValidLogin().then(response => {
-            console.log(response)
-        })
-    }, [])
-
-    useEffect(() => {
-        Backendless.UserService.getCurrentUser().then((response: any) => {
-            console.log(response)
-            setCurrentUser(response)
-        })
-    }, [])
-
-    return (
-        <div className="App">
-            <header className="App-header">
-
-                <div style={{marginBottom: "10px"}}>Registration</div>
-                <input className="form-control" style={{width: "15%", marginBottom: "10px"}} placeholder="email"
-                       onChange={(e) => setEmail(e.target.value)} value={email}/>
-                <input className="form-control" style={{width: "15%", marginBottom: "10px"}} placeholder="password"
-                       onChange={(e) => setPassword(e.target.value)} value={password}/>
-                <input className="form-control" style={{width: "15%", marginBottom: "10px"}} placeholder="name"
-                       onChange={(e) => setName(e.target.value)} value={name}/>
-                <input className="form-control" style={{width: "15%", marginBottom: "10px"}} placeholder="age"
-                       onChange={(e) => setAge(e.target.value)} value={age}/>
-                <input className="form-control" style={{width: "15%", marginBottom: "10px"}} placeholder="country"
-                       onChange={(e) => setCountry(e.target.value)} value={country}/>
-                <input className="form-control" style={{width: "15%", marginBottom: "10px"}} placeholder="gender"
-                       onChange={(e) => setGender(e.target.value)} value={gender}/>
-                <button onClick={reg}>Register</button>
-
-
-                <div>Login</div>
-                <input className="form-control" style={{width: "15%", marginBottom: "10px"}} placeholder="email"
-                       onChange={(e) => setEmailLog(e.target.value)} value={emailLog}/>
-                <input className="form-control" style={{width: "15%", marginBottom: "10px"}} placeholder="password"
-                       onChange={(e) => setPasswordLog(e.target.value)} value={passwordLog}/>
-                <button onClick={log}>Login</button>
-
-
-                <input className="form-control" style={{width: "15%", marginBottom:"10px", marginTop: "30px"}} placeholder="email"
-                       onChange={(e) => setEmail(e.target.value)} value={email}/>
-                <button style ={{marginBottom:"20px"}}onClick={help}>Restore password</button>
-
-                {image &&
-                <img src={URL.createObjectURL(image)} style={{width: "100px", height: "100px", objectFit: "contain"}}
-                     alt="product-img"/>
-                }
-                <input type="file" onChange={
-                    (e) => setImage(e.target.files && e.target.files[0])
-                }/>
-                <button onClick={handleFile}>send file</button>
-
-                <div>Create folder</div>
-                <input placeholder="name folder/file" value={nameFolder}
-                       onChange={(e) => setNameFolder(e.target.value)}/>
-                <button onClick={createFolder}>Create folder</button>
-
-                <div>Delete folder/file by name</div>
-                <input placeholder="delete folder" value={nameFolderDelete}
-                       onChange={(e) => setNameFolderDelete(e.target.value)}/>
-                <button onClick={deleteFolder}>Delete</button>
-
-                <button onClick={fetchList}>get list</button>
-                <div style={{display: "flex", alignItems: "center"}}>
-                    {list?.map((item: any) =>
-                        <div>
-                            <iframe src={item.publicUrl} style={{width: "200px", height: "200px"}}/>
-                            <a href={item.publicUrl}>{item.name}</a>
-                        </div>
-                    )}
-                </div>
-
-            </header>
-
+  return (
+    <div className="App">
+      <header className="App-header">
+        {image &&
+          <img src={URL.createObjectURL(image)} style={{ width: "100px", height: "100px", objectFit: "contain" }}
+            alt="product-img" />
+        }
+        <div className="mb-3" style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'flex-start',
+        }}>
+          <label className="form-label">Default file input example</label>
+          <input style={{ marginBottom: 10 }} className="form-control" type="file" onChange={
+            (e) => setImage(e.target.files && e.target.files[0])
+          } />
+          <button style={{ width: '100%' }} type="button" className="btn btn-primary" onClick={handleFile}>SEND FILE</button>
         </div>
-    );
+
+        <div className="mb-3" style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'flex-start',
+        }}>
+          <label className="form-label">Create folder</label>
+          <input style={{ marginBottom: 10 }} className="form-control" placeholder="name folder/file" onChange={(e) => setNameFolder(e.target.value)} value={nameFolder} />
+          <button style={{ width: '100%' }} type="button" className="btn btn-primary" onClick={createFolder}>CREATE FOLDER</button>
+        </div>
+
+        <div className="mb-3" style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'flex-start',
+        }}>
+          <label className="form-label">Delete folder/file by name</label>
+          <input style={{ marginBottom: 10 }} className="form-control" placeholder="name folder/file" value={nameFolderDelete}
+            onChange={(e) => setNameFolderDelete(e.target.value)} />
+          <div className="d-flex justify-content-around" style={{ width: '100%' }}>
+            <button className="btn btn-danger btn-sm" type="button" onClick={deleteFolder}>DELETE</button>
+            <button className="btn btn-info btn-sm text-light" type="button" onClick={fetchList}>GET LIST</button>
+          </div>
+        </div>
+
+        <div style={{ display: "flex", alignItems: "center" }}>
+          {list?.map((item: any) =>
+            <div>
+              <iframe src={item.publicUrl} style={{ width: "200px", height: "200px" }} />
+              <a href={item.publicUrl}>{item.name}</a>
+            </div>
+          )}
+        </div>
+      </header>
+    </div>
+  );
 };
 
 export default Home;
